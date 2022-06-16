80374609003a32a6000a415cbe94afcbbf21826b
jalapate <jalakp19@gmail.com>
2022-06-16 05:16:03 -0700

    working code for sip, rtt and latency

--- examples/ringbuffer/ringbuffer.c
+++ examples/ringbuffer/ringbuffer.c
@@ -7,11 +7,6 @@
 #include "bpf_endian.h"
 
 #define AF_INET 2
-#define AF_INET6 10
-#define TASK_COMM_LEN 16
-
-const volatile __u64 targ_min_us = 0;
-const volatile int targ_tgid     = 0;
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
@@ -96,7 +91,7 @@ struct tcp_sock {
  * can generate a Go type from it.
  */
 
-// For TCP related information (RTT)
+// For TCP RTT
 struct tcpevent {
 	u8 comm[80];
 	u16 sport;
@@ -113,14 +108,18 @@ struct {
 
 const struct tcpevent *unused2 __attribute__((unused));
 
+// For TCP latency
+struct latdata {
+	u8 comm[80];
+	u64 delta_us;
+	u64 ts_us;
+};
 
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} latdatas SEC(".maps");
 
-
-//latency start
-
-
-
-
+const struct latdata *unusedld __attribute__((unused));
 
 struct piddata {
 	u8 comm[80];
@@ -135,135 +134,52 @@ struct {
 
 const struct piddata *unusedp __attribute__((unused));
 
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 4096);
-	__type(key, struct sock *);
-	__type(value, struct piddata);
-} start SEC(".maps");
-
-struct latevent {
-	union {
-		u32 saddr_v4;
-	};
-	union {
-		u32 daddr_v4;
-	};
-	u8 comm[80];
-	u64 delta_us;
-	u64 ts_us;
-	u32 tgid;
-	int af;
-	u16 lport;
-	u16 dport;
+struct bpf_map_def SEC("maps") start = {
+	.type        = BPF_MAP_TYPE_HASH,
+	.key_size    = sizeof(struct sock *),
+	.value_size  = sizeof(struct piddata),
+	.max_entries = 4096,
 };
-
-struct {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 1 << 24);
-} latevents SEC(".maps");
-
-const struct latevent *unused3 __attribute__((unused));
-
-// struct {
-// 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-// 	__uint(key_size, sizeof(u32));
-// 	__uint(value_size, sizeof(u32));
-// } latevents SEC(".maps");
 
 SEC("kprobe/tcp_connect")
 int BPF_KPROBE(tcp_connect, struct sock *sk) {
 	u32 tgid               = bpf_get_current_pid_tgid() >> 32;
 	struct piddata piddata = {};
 
-	if (targ_tgid && targ_tgid != tgid)
-		return 0;
-
 	bpf_get_current_comm(&piddata.comm, sizeof(piddata.comm));
 	piddata.ts   = bpf_ktime_get_ns();
 	piddata.tgid = tgid;
-	bpf_map_update_elem(&start, &sk, &piddata, 0);
-	bpf_printk("Hello");
-
-	struct piddata *pd;
-	pd = bpf_ringbuf_reserve(&piddatas, sizeof(struct piddata), 0);
-	if (!pd) {
-		return 0;
-	}
-
-	bpf_get_current_comm(&pd->comm, 80);
-	pd->ts   = bpf_ktime_get_ns();
-	pd->tgid = tgid;
-	bpf_ringbuf_submit(pd, 0);
+	bpf_map_update_elem(&start, &sk, &piddata, BPF_ANY);
 
 	return 0;
 }
 
 SEC("kprobe/tcp_rcv_state_process")
 int BPF_KPROBE(tcp_rcv_state_process, struct sock *sk) {
-	sk = (struct sock *)(PT_REGS_PARM1(ctx));
-
-	struct piddata *pd;
-	// s64 delta;
-	// u64 ts;
-
-	struct latevent *lat_event;
-	lat_event = bpf_ringbuf_reserve(&latevents, sizeof(struct latevent), 0);
-	if (!lat_event) {
-		return 0;
-	}
-
-	// if (sk->__sk_common.skc_state != 2)
-	// 	return 0;
-
-	pd = bpf_map_lookup_elem(&start, &sk);
+	struct piddata *pd = bpf_map_lookup_elem(&start, &sk);
 	if (!pd)
 		return 0;
 
-	// ts    = bpf_ktime_get_ns();
-	// delta = (s64)(ts - piddatap->ts);
-	// if (delta < 0) {
-	// 	bpf_map_delete_elem(&start, &sk);
-	// 	return 0;
-	// }
+	struct latdata ld = {};
+	s64 delta;
+	u64 ts;
 
-	bpf_get_current_comm(&lat_event->comm, 80);
-	lat_event->delta_us = 1000;
+	ts    = bpf_ktime_get_ns();
+	delta = (s64)(ts - pd->ts);
+	if (delta < 0) {
+		bpf_map_delete_elem(&start, &sk);
+		return 0;
+	}
 
-	// lat_event->delta_us = delta / (u64)1000;
-	// 	if (targ_min_us && lat_event->delta_us < targ_min_us)
-	// 		goto cleanup;
-	// 	bpf_get_current_comm(&lat_event->comm, sizeof(lat_event->comm));
-	// 	lat_event->ts_us = ts / 1000;
-	// 	lat_event->tgid  = piddatap->tgid;
-	// 	lat_event->lport = sk->__sk_common.skc_num;
-	// 	lat_event->dport = sk->__sk_common.skc_dport;
-	// 	lat_event->af    = sk->__sk_common.skc_family;
-	// 	if (lat_event->af == AF_INET) {
-	// 		lat_event->saddr_v4 = sk->__sk_common.skc_rcv_saddr;
-	// 		lat_event->daddr_v4 = sk->__sk_common.skc_daddr;
-	// 	} else {
-	// 		// latevent->saddr_v6 = sk->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32;
-	// 		// latevent->daddr_v6 = sk->__sk_common.skc_v6_daddr.in6_u.u6_addr32;
-	// 	}
-	// 	// bpf_perf_event_output(ctx, &latevents, BPF_F_CURRENT_CPU, &latevent, sizeof(latevent));
-	bpf_ringbuf_submit(lat_event, 0);
+	bpf_get_current_comm(&ld.comm, 80);
+	ld.delta_us = delta / 1000;
+	ld.ts_us    = ts / 1000;
+
+	bpf_perf_event_output(ctx, &latdatas, BPF_F_CURRENT_CPU, &ld, sizeof(ld));
 
 	bpf_map_delete_elem(&start, &sk);
 	return 0;
 }
-
-
-
-
-//latency end
-
-
-
-
-
-
-
 
 // If this code is run on a server, then sendto will send 180 Ringing, 200 OK, 200 OK
 SEC("kprobe/__sys_sendto")
@@ -345,7 +261,7 @@ int BPF_PROG(tcp_close, struct sock *sk) {
 	tcp_info->sport = sk->__sk_common.skc_num;
 
 	tcp_info->srtt = ts->srtt_us >> 3;
-	tcp_info->srtt /= 1000;
+	// tcp_info->srtt /= 1000;
 
 	bpf_ringbuf_submit(tcp_info, 0);
 
